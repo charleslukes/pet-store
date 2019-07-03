332c5806ce7e74982f39e3b28b038857fa94342a
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-03 12:04:28 +0100

    connected files in the app,.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './App.css';
+import NavBar from './components/common/NavBar';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
