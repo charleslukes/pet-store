f79dfc849ce6c94dc8ac41349520b2e9e018e0a3
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-03 12:03:30 +0100

    created the nav bar

---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './NavBar.css'
+
+const NavBar = () => {
+
+
+  return (
+    <div>
+      <ul className='bar-titles'>
+        <li className='icon'>9pets</li>
+        <li>About</li>
+        <li>Search Pets</li>
+        <li>Blog</li>
+        <li>Contact</li>
+        <li>Sign in/ Register</li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar;
