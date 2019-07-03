de68b95552afbe96744e92c1afa4061794fbb36c
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-03 13:05:15 +0100

    added the footer component

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+
+  return (
+    <div className='footer'>
+      <div className='about'>
+        <div className='first'>About us</div>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Molestias velit ex distinctio, quisquam debitis
+          quidem incidunt? Voluptate labore perspiciatis, 
+          blanditiis
+        </div>
+      </div>
+
+      <div className='contact'>
+        <div className='first'>Contact</div>
+        <div><label htmlFor="phone">Phone:</label>0123456789</div>
+        <div><label htmlFor="email">Email:</label>example@gmail.com</div>
+      </div>
+      <div className='searchBar'>
+        <div className='first'>Search</div>
+        <input type="text" placeholder="Search.." name="search" />
+      </div>
+    </div>
+  )
+
+}
+
+export default Footer;
