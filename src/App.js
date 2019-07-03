816bf31c494a3a048d97cb96dbb92740f7a23aae
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-03 17:56:18 +0100

    add raleway font to index filr

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/common/NavBar';
 import Footer from './components/common/Footer';
+import Header from './components/common/Header'
 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Header />
       <Footer />
     </div>
   );
