36add8343c5bd7cd0effacb6b4ddc48e803fa487
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-04 03:23:27 +0100

    add more tags to the landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import NavBar from './common/NavBar';
 import Footer from './common/Footer';
 import Header from './common/Header'
+import SalesCard from './SalesCard';
 import './LandingPage.css'
 
 
 const LandingPage = () => {
-
+    const fake = [1,2,3,4,5,6]
   return (
     <div className='main-landing'>
       <div className='landing-nav'>
@@ -18,7 +19,7 @@ const LandingPage = () => {
       <section>
         <div className='info-section'> 
             <p className='info-title'>Meet your Friend</p>
-            <p>
+            <p className='main-info'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Quam necessitatibus doloremque velit libero architecto fuga est, 
             quaerat delectus quae? Laboriosam ipsam, nobis dignissimos 
@@ -28,8 +29,13 @@ const LandingPage = () => {
 
       </section>
       <section className='landing-sales'>
-        <p>Get a Pet Today</p>
-
+        <p className='landing-sales-title'>Get a Pet Today</p>
+        <div>
+          <div className='sales-card'>
+            {fake.map(elem => <SalesCard />)}
+          </div>
+          <div className='sales-btn'> <button>view more</button></div>
+        </div>
       </section>
       <Footer />
     </div>
