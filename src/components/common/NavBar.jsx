3ff986d6f2ffc7c2f69be46b7c88851e24217c5a
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-06 15:49:12 +0100

    change the structure of the navbar

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './NavBar.css'
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 
@@ -7,12 +8,12 @@ const NavBar = () => {
   return (
     <div className='main-nav'>
       <ul className='bar-titles'>
-        <li className='icon'>9pets</li>
-        <li>About</li>
-        <li>Search Pets</li>
-        <li>Blog</li>
-        <li>Contact</li>
-        <li>Sign in/ Register</li>
+        <li className='icon '> <Link className='icon-link' to='/landingpage'>9pets</Link></li>
+        <li><Link className='link' to='/aboutpage'>About</Link></li>
+        <li><Link className='link' to='searchpage'>Search Pets</Link></li>
+        <li><Link className='link' to='blogpage'>Blog</Link></li>
+        <li className='style-li'><Link className='link' to='contactpage'>Contact</Link></li>
+        <li className='style-li'><Link className='link' to='registerpage'>Sign in/ Register</Link></li>
       </ul>
     </div>
   )
