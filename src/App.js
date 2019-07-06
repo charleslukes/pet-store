592308c8a634cba140cef1719fb7b168b3275a4b
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-06 15:28:24 +0100

    add route for all pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './App.css';
-import LandingPage from './components/LandingPage';
+import NavBar from './components/common/NavBar';
+import Footer from './components/common/Footer'
+import SearchPage from './components/SearchPage/SearchPage';
+import BlogPage from './components/BlogPage/BlogPage';
+import LandingPage from './components/Landing/LandingPage';
+import SalesInfo from './components/SalesInfo/SalesInfo'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <Router>
+      <div className="App">
+        <div className='nav-div'>
+        <NavBar />
+        </div>
+        <Switch>
+          <Route exact path='/landingpage' component={LandingPage} />
+          <Route path='/searchpage' component={SearchPage} />
+          <Route path='/blogpage' component={BlogPage} />
+          <Route path='/salesinfo' component={SalesInfo} />
+        </Switch>
+        <div className='foot-div'>
+        <Footer />
+        </div>
+      </div>
+    </Router>
   );
 }
 
