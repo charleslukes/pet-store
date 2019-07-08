2f01dede670b231c4732ecbe80c789b97bdf8ad6
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-08 10:35:29 +0100

    added contacts info to sales info jsx

--- src/components/SalesInfo/SalesInfo.jsx
+++ src/components/SalesInfo/SalesInfo.jsx
@@ -12,14 +12,14 @@ const SalesInfo = () => {
         </div>
         <div className='sales-social'>
           <div className='sales-icon'>
-          <span><i className="far fa-eye"></i></span>
-          <span><i className="far fa-heart"></i></span>
-          <span><i className="far fa-comments"></i></span>
+            <span><i className="far fa-eye"></i></span>
+            <span><i className="far fa-heart"></i></span>
+            <span><i className="far fa-comments"></i></span>
           </div>
         </div>
         <div className='sales-info'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim odio recusandae
-          incidunt eaque porro, quos et rem necessitatibus reiciendis aliquid molestiae hic 
+          incidunt eaque porro, quos et rem necessitatibus reiciendis aliquid molestiae hic
           quia exercitationem sed nemo consectetur vel ullam asperiores.
         </div>
         <div className='sales-share'>
@@ -29,9 +29,9 @@ const SalesInfo = () => {
       <div className='sales-contact'>
         <p className='sales-contact-title'>9pets</p>
         <p className='sales-contact-location'>Nigeria| Lagos | Enugu</p>
-        <div className='sales-contact-info'> 
-            <p className='sales-number'><div className='sales-icon1'><i className="fas fa-mobile-alt"></i></div>Phone Number</p>
-            <p className='sales-person'><div className='sales-icon1'><i className="fas fa-info-circle"></i></div>Contact Person</p>
+        <div className='sales-contact-info'>
+          <p className='sales-number'><div className='sales-icon1'><i className="fas fa-mobile-alt"></i></div>Phone Number</p>
+          <p className='sales-person'><div className='sales-icon1'><i className="fas fa-info-circle"></i></div>Contact Person</p>
         </div>
       </div>
     </div>
