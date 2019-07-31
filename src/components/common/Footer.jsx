ab05e72e82a8362ffd961d780a36b747202a5be5
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-31 02:03:38 +0100

    improved footer to add links

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,27 +4,44 @@ import './Footer.css';
 const Footer = () => {
 
   return (
-    <div className='footer'>
+    <footer>
+      <div className='footer'>
       <div className='about'>
         <div className='first'>About us</div>
         <div className='about-text'>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
           Molestias velit ex distinctio, quisquam debitis
-          quidem incidunt? Voluptate labore perspiciatis, 
-          blanditiis
         </div>
       </div>
 
       <div className='contact'>
-        <div className='first'>Contact</div>
-        <div><label htmlFor="phone">Phone:</label>0123456789</div>
-        <div><label htmlFor="email">Email:</label>example@gmail.com</div>
+        <div className='first'>Contact Us</div>
+        <div><label htmlFor="phone"><i class="fas fa-phone"></i></label>0123456789</div>
+        <div><label htmlFor="email"><i class="fas fa-inbox"></i></label>example@gmail.com</div>
       </div>
       <div className='searchBar'>
         <div className='first'>Search</div>
         <input type="text" placeholder="Search.." name="search" />
       </div>
-    </div>
+      </div>
+      <div className='last-foot-row'>
+        <p>9Pets - All rights reserved</p>
+        <ul>
+          <li>Home</li>
+          <li>Blog</li>
+          <li>Contact</li>
+          <li>Terms and Conditions</li>
+        </ul>
+        <div>
+          <ul>
+            <li>Follow us</li>
+            <li><i class="fab fa-facebook"></i></li>
+            <li><i class="fab fa-twitter"></i></li>
+            <li><i class="fab fa-instagram"></i></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   )
 
 }
